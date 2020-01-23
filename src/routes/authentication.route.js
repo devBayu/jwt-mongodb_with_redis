7936904c9c66f0bfd7fb04091426cfa51db2ee4b
bayurahman20 <bayurahman497@gmail.com>
2020-01-24 06:06:11 +0700

    [IMPROVE] authentication.route

---
+++ src/routes/authentication.route.js
@@ -0,0 +1,21 @@
+import {Router} from 'express';
+import AuthenticationService from "../service/authentication.service";
+import authentication from "../middleware/authentication.middleware";
+
+let authenticationService = new AuthenticationService();
+const authenticationRouter = Router()
+    .post("/login", async (req, res) => {
+        try {
+            const {username, password} = req.body;
+            const result = await authenticationService.login(username, password);
+            return res.status(200).json({
+                type: 'Bearer',
+                status: true,
+                token: result
+            });
+        } catch (e) {
+            res.status(400).json({message: 'Invalid Credentials'})
+        }
+    });
+
+export default authenticationRouter;
