862a18ee5e1cb76a591ab4a0323b5ebb8415efb0
bayurahman20 <bayurahman497@gmail.com>
2020-01-24 02:18:31 +0700

    [FEATURE] Able to complete message constant

---
+++ src/constants/message.constant.js
@@ -0,0 +1,3 @@
+export const ID_NOT_FOUND = "Cant be found id";
+export const DELETE_FAILED = "Delete Failed Can't be found id or employee success deleted";
+export const USERNAME_NOT_FOUND = "Cant be found username";
